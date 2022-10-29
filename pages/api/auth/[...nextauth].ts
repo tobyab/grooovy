bca4e5c60ddffd719aad804e9074959746233494
Toby <mrtobez08@gmail.com>
2022-10-29 21:42:19 +0100

    changes made :D

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,10 +2,10 @@ import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 
 export default NextAuth({
-    providers: [
-        GithubProvider({
-            clientId: process.env.GITHUB_ID,
-            clientSecret: process.env.GITHUB_SECRET,
-        }),
-    ],
+        providers: [
+            GithubProvider({
+                clientSecret: process.env.GITHUB_SECRET,
+                clientId: process.env.GITHUB_ID,
+            }),
+        ],
 })
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,11 @@
 import type { NextPage } from "next"
 import Head from "next/head"
 import Link from "next/link"
-import { Logo } from "../components/icons"
 
 const Home: NextPage = () => {
+    console.log("hey", process.env.GITHUB_ID)
   return (
       <div className="m-8">
-          <Logo/>
           <div className="grid mt-64 place-items-center space-y-4">
             <Head>
                 <title>Grooovy</title>
@@ -14,7 +13,7 @@ const Home: NextPage = () => {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
               <div>
-                  <h1 className="text-5xl font-medium w-3/4">Literally the easiest way to manage your code!</h1>
+                  <h1 className="text-5xl font-medium w-3/4">Literally the groooviest way to manage your code!</h1>
                   <h2 className="text-xl w-1/2 mt-8">Coding is hard.
                       Especially when no one has any idea about what&apos;s going on!
                       Grooovy aims to make coding easier. For everyone.
@@ -25,7 +24,7 @@ const Home: NextPage = () => {
                       <button
                             className="rounded-lg p-4 bg-black text-white mt-8"
                             >
-                          Let&apos;s get groovin&apos;!
+                          Ready to get groovin&apos;?
                       </button>
                   </Link>
               </div>
